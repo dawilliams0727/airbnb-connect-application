de269bd1c3e18c8cd53704d0d839e3dc10ffa270
David Williams <109742173+phenomenon1995@users.noreply.github.com>
2025-02-02 16:02:33 -0500

    intialized vue with linting and basic UI (nav and routing)

---
+++ airbnb-connect-app/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>I'm excited to join the Connect Program!</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ airbnb-connect-app/src/views/ProjectsView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Projects</h1>
+        <p>Here are some of the projects I have worked on:</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
