d92a056923efdfec48a1ccee70d8d92485b4eb93
David Williams <109742173+phenomenon1995@users.noreply.github.com>
2025-02-04 08:36:17 -0500

    initialized spring and added animations to front end

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,12 +1,52 @@
 <template>
     <div>
         <h1>Projects</h1>
-        <p>Here are some of the projects I have worked on:</p>
+        <div class="test">
+        <TransitionGroup name="slide" tag="div">
+            <div v-for="(project) in projects" 
+            :key="project.name" 
+            style="list-style-type: none;">
+                <project-card :project="project"></project-card>
+            </div>
+        </TransitionGroup>
+        </div>
     </div>
 </template>
 
 <script>
+import testService from '../services/testService';
+import ProjectCard from '../components/ProjectCard.vue';
+export default {
+    components: {
+        ProjectCard
+    },
+    name: 'ProjectsView',
+    data() {
+        return {
+            projects: []
+        };
+    },
+    created() {
+        // Fetch projects from the server
+        testService.getProjects().then(response => {
+            this.projects = response.data;
+        });
+    }
+};
 </script>
 
 <style>
+    .test{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 20px;
+        
+    }
+    .slide-enter-from{
+        opacity: 0;
+        transform: translateY(30px);
+    }
+    .slide-enter-active, .slide-leave-active {
+        transition: transform 0.5s ease;
+    }
 </style>
